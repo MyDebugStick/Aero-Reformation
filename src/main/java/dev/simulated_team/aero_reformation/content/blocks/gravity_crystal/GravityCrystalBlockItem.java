78c8a1d6079ee0f8a5724090ddbcb250195c91ee
MyDebugStick <mydebugstick@github.com>
2026-06-14 14:02:59 +0800

    Add gravity crystal drag warning tooltip

--- src/main/java/dev/simulated_team/aero_reformation/content/blocks/gravity_crystal/GravityCrystalBlockItem.java
+++ src/main/java/dev/simulated_team/aero_reformation/content/blocks/gravity_crystal/GravityCrystalBlockItem.java
@@ -4,11 +4,21 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.block.Block;
+
+import java.util.List;
 
 public class GravityCrystalBlockItem extends BlockItem {
     public GravityCrystalBlockItem(Block block, Properties properties) {
         super(block, properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltip, TooltipFlag flag) {
+        super.appendHoverText(stack, context, tooltip, flag);
+        tooltip.add(Component.translatable("tooltip.aero_reformation.gravity_crystal.drag_warning")
+                .withStyle(ChatFormatting.DARK_PURPLE));
     }
 
     @Override
